cb7446225c82db32eecfad173a5bdf7c0842b714
ddavidov <ddavidov@mindk.com>
2016-11-10 12:51:22 +0200

    Init

---
+++ administrator/logs/joomla_update.php
@@ -0,0 +1,16 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2016-11-08 09:05:25 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2016-11-08T09:05:25+00:00	INFO 127.0.0.1	update	Update started by user Super User (190). Old version is 3.6.2.
+2016-11-08T09:05:25+00:00	INFO 127.0.0.1	update	Downloading update file from https://downloads.joomla.org/cms/joomla3/3-6-4/joomla_3-6-4-stable-update_package-zip?format=zip&jcompat=my.zip.
+2016-11-08T09:05:50+00:00	INFO 127.0.0.1	update	File joomla_3-6-4-stable-update_package-zip?format=zip&jcompat=my.zip successfully downloaded.
+2016-11-08T09:05:50+00:00	INFO 127.0.0.1	update	Starting installation of new version.
+2016-11-08T09:05:54+00:00	INFO 127.0.0.1	update	Finalising installation.
+2016-11-08T09:05:54+00:00	INFO 127.0.0.1	update	Ran query from file 3.6.3-2016-08-15. Query text: ALTER TABLE `#__newsfeeds` MODIFY `link` VARCHAR(2048) NOT NULL;.
+2016-11-08T09:05:54+00:00	INFO 127.0.0.1	update	Ran query from file 3.6.3-2016-08-16. Query text: INSERT INTO `#__postinstall_messages` (`extension_id`, `title_key`, `description.
+2016-11-08T09:05:54+00:00	INFO 127.0.0.1	update	Deleting removed files and folders.
+2016-11-08T09:05:55+00:00	INFO 127.0.0.1	update	Cleaning up after installation.
+2016-11-08T09:05:55+00:00	INFO 127.0.0.1	update	Update to version 3.6.4 is complete.
---
+++ joomla-env-bootstrap.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @package    Joo.Test
+ *
+ */
+
+/**
+ * Define the application's minimum supported PHP version as a constant so it can be referenced within the application.
+ */
+define('JOOMLA_MINIMUM_PHP', '5.3.10');
+
+if (version_compare(PHP_VERSION, JOOMLA_MINIMUM_PHP, '<'))
+{
+    die('Your host needs to use PHP ' . JOOMLA_MINIMUM_PHP . ' or higher to run this version of Joomla!');
+}
+
+/**
+ * Constant that is checked in included files to prevent direct access.
+ * define() is used in the installation folder rather than "const" to not error for PHP 5.2 and lower
+ */
+define('_JEXEC', 1);
+
+if (file_exists(__DIR__ . '/defines.php'))
+{
+    include_once __DIR__ . '/defines.php';
+}
+
+if (!defined('_JDEFINES'))
+{
+    define('JPATH_BASE', __DIR__);
+    require_once JPATH_BASE . '/includes/defines.php';
+}
+
+require_once JPATH_BASE . '/includes/framework.php';
+
+// Instantiate the application.
+$app = JFactory::getApplication('site');
+
+// Bootstrap ZOO:
+require_once( JPATH_ADMINISTRATOR . '/components/com_zoo/config.php');
+
+// get zoo instance
+$zoo = App::getInstance('zoo');
